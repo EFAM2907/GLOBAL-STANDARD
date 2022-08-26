5ee34b0da78f10e43adbc9f82290464674eb2f88
efam2907 <efam2907@gmail.com>
2022-08-26 02:24:41 -0500

    foto principal de home

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>F</div>
+    <div>Footer</div>
   )
 }
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -3,12 +3,23 @@ import Nav from '../component/nav'
 import Footer from '../component/footer'
 import Products from '../component/products'
 import Services from '../component/services'
+import imagen from '../img/BannerPrincipal.png'
+import s from '../styles/home.module.css'
 
 
 export default function Home() {
   return (
     <div>
     <Nav />
+    <div className={s.container}>
+        
+     <img className={s.img} src={imagen} alt='imagen-Principal'  />
+    <div className={s.alltext}>
+      <h1 className={s.text}>WELCOME TO INBUILD</h1>
+       <h2 className={s.texto1} >GLOBAL STANDARD</h2>
+       <h3 className={s.texto2} >BUILD ALL THE UNIQUE PATTERNS</h3>
+     </div>
+    </div>
     <Services />
     <Products />
     <Footer />
