c669e1e7fc2999b46acfead00c240f98b406e0e5
efam2907 <efam2907@gmail.com>
2022-08-26 04:04:00 -0500

    products

--- src/component/products.jsx
+++ src/component/products.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import s from '../styles/products.module.css'
+import Json from '../extras/product.json'
 
 export default function Products() {
+    const products = Json.data
+
+    console.log(products)
+
+ function Card({name, img, price}){
+    return(
+
+    <div>
+        <h2>{name}</h2>
+        <img src={img} alt='precio' width='150px' />
+        <p>{price}</p>
+    </div>
+    )
+ }
+
   return (
-    <div>Products</div>
+    <div >
+        <div className={s.container}>
+
+        {
+            products.map(p => (
+                <div  key={p.name}>
+                    <Card 
+                  name={p.name}
+                  img={p.img}
+                  price={p.price} />
+                </div>
+
+            ))
+        }
+        </div>
+    </div>
   )
 }
