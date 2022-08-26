b5653a7759f5987ba1744a2912a463a2862321cb
efam2907 <efam2907@gmail.com>
2022-08-26 00:06:51 -0500

    componentes a trabajar

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Nav from '../component/nav'
+import Footer from '../component/footer'
+import Products from '../component/products'
+import Services from '../component/services'
 
-export default function home() {
+
+export default function Home() {
   return (
-    <div>home</div>
+    <div>
+    <Nav />
+    <Services />
+    <Products />
+    <Footer />
+
+    </div>
   )
 }
